d1a5fdeb66bd9e9935ce530b21cc82acfe1090be
ksubero <143946424+ksubero@users.noreply.github.com>
2023-09-04 19:27:20 +0000

    Add files via upload

---
+++ KSubero_CW_1.cpp
@@ -0,0 +1,26 @@
+/*
+*Class: CMSC140 21909
+*Instructor: Grigoriy Grinberg
+*Classwork1
+*Description: Developing a program to display information
+*Due Date: 09/05/2023
+*I pledge that I have completed the programming assignment independently.
+* I have no copied the code from a student or any source.
+* Print your Name here: Keiffer Subero
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "********************" << endl;
+	cout << "Hello CMSC140 CRN 21909 class!" << endl;
+	cout << "My name is Keiffer Subero" << endl;
+	cout << "My major is Computer Science 107" << endl;
+	cout << "Programming is intimidating at first glance" << endl;
+	cout << "Today is September 5th."
+	cout << "********************" << endl;
+	system("pause");
+	return 0;
+}
