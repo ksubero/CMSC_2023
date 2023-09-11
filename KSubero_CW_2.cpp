e011fd4b7272cb9c5877ec332d1589915bd9311f
ksubero <143946424+ksubero@users.noreply.github.com>
2023-09-11 10:07:26 -0400

    Add files via upload

---
+++ KSubero_CW_2.cpp
@@ -0,0 +1,25 @@
+/*
+*Class: CMSC140 21909
+*Instructor: Grigoriy Grinberg
+*Classwork2
+*Description: Developing a program to display information
+*Due Date: 09/11/2023
+*I pledge that I have completed the programming assignment independently.
+* I have no copied the code from a student or any source.
+* Print your Name here: Keiffer Subero
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const string MY_NAME = "Keiffer Subero";
+	const string MY_MAJOR = "Computer Science";
+	string myAddress = "15809 Seurat Drive North Potomac MD 20878";
+	string myPhoneNumber = "3013568592";
+
+	cout << MY_NAME << endl << myAddress << endl << myPhoneNumber << endl << MY_MAJOR << endl;
+
+	return 0;
+}
